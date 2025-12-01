b53cb9d4d550b209a25ceef6c7f09404c865a418
SarveshNavale <sarveshnavale18@gmail.com>
2025-12-02 01:32:15 +0530

    flask and database setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
 import mysql.connector
+
 app = Flask(__name__)
 
 db = mysql.connector.connect(
@@ -8,3 +9,11 @@ db = mysql.connector.connect(
     password="#SAR1807",
     database="RotaryClub_Database"
 )
+
+@app.route('/')
+def home():
+    
+    return render_template("profile.html") # replace name of page here tht you are currently editing
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
