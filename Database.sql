d6fc2acecd19e41c2ba49cf795ececb60c56b534
SarveshNavale <sarveshnavale18@gmail.com>
2025-12-06 19:15:44 +0530

    database tables

--- Database.sql
+++ Database.sql
@@ -1,1 +1,34 @@
 create database RotaryClub_Database;
+
+CREATE TABLE cust_info (
+    cust_name   VARCHAR(100),
+    cust_number VARCHAR(15),
+    cust_age    INT,
+    cust_email  VARCHAR(100)
+);
+CREATE TABLE notification_info (
+    notif_date        DATE,
+    notif_time        TIME,
+    notif_heading     VARCHAR(250),
+    notif_description TEXT
+);
+CREATE TABLE Admin_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Admin_id VARCHAR(10),
+    OTP INT
+);
+INSERT INTO Admin_info (Admin_id, OTP)
+VALUES ('TC918710', 4085);       -- for TC
+INSERT INTO Admin_info (Admin_id, OTP)     -- for complaint checkerr
+VALUES ('ADM918710', 4085);
+
+
+CREATE TABLE complaints (
+    idc INT AUTO_INCREMENT PRIMARY KEY,   
+    comp_time     TIME,                  
+    comp_generator VARCHAR(100),         
+    comp_gen_no   VARCHAR(20),           
+    comp_status   VARCHAR(50)            
+);
+
+
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="hrishi@123",
+    password="#SAR1807",
     database="RotaryClub_Database"
 )
 
