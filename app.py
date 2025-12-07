63eb7d55845c56b9ccdbd08530358a254ef1bf7b
SarveshNavale <sarveshnavale18@gmail.com>
2025-12-07 16:56:22 +0530

    complete dino rush game

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, send_from_directory
 import mysql.connector
 
 app = Flask(__name__)
@@ -12,8 +12,15 @@ db = mysql.connector.connect(
 
 @app.route('/')
 def home():
-    
-    return render_template("robo.html") # replace name of page here tht you are currently editing
+    return render_template("play.html")
+
+@app.route('/dino')
+def dino():
+    return render_template("dino-rush.html")
+
+@app.route('/static/games/dino/<path:filename>')
+def serve_dino_files(filename):
+    return send_from_directory('static/games/dino', filename)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
