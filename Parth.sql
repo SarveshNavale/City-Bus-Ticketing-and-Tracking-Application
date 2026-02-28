046fe922493d77323c9f92453b3e426136f0b23e
ParthRahate21 <parthrahate5@gmail.com>
2026-02-28 10:22:17 +0530

    parth.sql added

---
+++ Parth.sql
@@ -0,0 +1,40 @@
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('City Bus Stand', 1, 16.9914520, 73.2950390);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Malnaka', 1, 16.9906412, 73.3059455);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Maruti Mandir (Kokan)', 1, 16.9908009, 73.3120636);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Godbole', 1, 16.9950168, 73.3190417);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Charmalay', 1, 16.9964154, 73.3208352);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Kokan Nagar', 1, 17.0016121, 73.3245508);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Radha Krishna Nagar', 1, 17.0035718, 73.3269559);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Aadishti', 1, 17.0099843, 73.3301266);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Finolex', 1, 17.0116948, 73.3353763);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Jambhul Phata', 1, 17.0173015, 73.3356863);
+
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('City Bus Stand', 2, 16.9914520, 73.2950390);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Malnaka', 2, 16.9906412, 73.3059455);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Maruti Mandir (Main Highway)', 2, 16.9904574, 73.3130624);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Shivaji Nagar', 2, 16.9913144, 73.3213231);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Salvi', 2, 16.9940142, 73.3273839);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('JK Files', 2, 16.9955833, 73.3309237);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Railway Station', 2, 16.9980804, 73.3578163);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Mahalakshmi', 2, 17.0015264, 73.3720352);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Khedshi', 2, 17.0139045, 73.3949133);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Hatkhamba', 2, 17.0150127, 73.4054062);
+
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('City Bus Stand', 3, 16.9914520, 73.2950390);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Malnaka', 3, 16.9906412, 73.3059455);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Maruti Mandir (Nachane Road)', 3, 16.9897810, 73.3128478);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Jogalekar', 3, 16.9897621, 73.3145604);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Power House', 3, 16.9885660, 73.3178680);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('I.T.I', 3, 16.9867141, 73.3230856);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Nachane', 3, 16.9853803, 73.3291087);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Godown', 3, 16.9865191, 73.3334559);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Shantinagar', 3, 16.9845721, 73.3396431);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Kajarghati', 3, 16.9769149, 73.3674380);
+
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('City Bus Stand', 4, 16.9914520, 73.2950390);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Malnaka', 4, 16.9906412, 73.3059455);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Maruti Mandir (Main Highway)', 4, 16.9904574, 73.3130624);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Shivaji Nagar', 4, 16.9913144, 73.3213231);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Salvi', 4, 16.9940142, 73.3273839);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('JK Files', 4, 16.9955833, 73.3309237);
+INSERT INTO stops_info (stop_name, track_no, latitude, longitude) VALUES ('Railway Station', 4, 16.9980804, 73.3578163);
