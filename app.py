8c384b73304a92bbb7c777860961c8b7e498b4c5
ParthRahate21 <parthrahate5@gmail.com>
2026-03-11 19:19:39 +0530

    tourist page added

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_db():
         host="localhost",
         user="root",
 
-        password="#SAR1807",
+        password="Parth@123",
 
         database="RotaryClub_Database"
     )
@@ -1606,4 +1606,9 @@ def delete_complaint(id):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+@app.route("/tourist")
+def tourist():
+    return render_template("tourist.html")
 #task completed
